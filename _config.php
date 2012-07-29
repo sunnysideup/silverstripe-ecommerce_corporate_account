aae82a399c916a7d93e145fb00bccc5a9fe47e19
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-07-29 14:53:30 +0000

    ENHANCEMENT: removing old _configs that can now be set in ecommerce.yaml
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_corporate_account/trunk@3119 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -9,7 +9,14 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START ecommerce_corporate_account MODULE ----------------===================
 //MUST SET... WHERE APPLICABLE
-//Order::add_modifier("OrderMarker");
+/**
+ * ADD TO ECOMMERCE.YAML:
+Order:
+	modifiers: [
+		...
+		OrderMarker
+	]
+*/
 //Object::add_extension("EcommerceDatabaseAdmin", "CreateEcommerceApprovedCustomerGroup_AdminDecorator");
 //Object::add_extension("Product", "EcommerceCorporateGroupBuyableDecorator");
 //Object::add_extension("ProductVariation", "EcommerceCorporateGroupBuyableDecorator");
