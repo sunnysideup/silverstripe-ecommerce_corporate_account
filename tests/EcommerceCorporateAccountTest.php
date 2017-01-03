90c9f6bf2a32c368b7f5d0fe4efdb6e5ac516b9f
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:04:31 +1300

    PATCH: module clean-up

---
+++ tests/EcommerceCorporateAccountTest.php
@@ -0,0 +1,13 @@
+<?php
+class EcommerceCorporateAccountTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
